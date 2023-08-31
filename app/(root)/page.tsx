2a82f3b2995125c0c0fefe621bffda37ddad684a
pjleb90 <pjleb90@gmail.com>
2023-08-31 19:50:44 -0400

    setup environment

---
+++ app/(root)/page.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+      <p>
+        Hello admin dashboard.
+      </p>
+        <Button className="p-4 bg-slate-400">
+          Click me
+        </Button>
+      </div>
+  )
+}
