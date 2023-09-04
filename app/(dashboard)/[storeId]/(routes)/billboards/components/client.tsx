63d5271db020154d3c84d1728f06aeb959e86bcb
pjleb90 <pjleb90@gmail.com>
2023-09-04 14:48:43 -0400

    created data table to view and edit billboards

--- app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
@@ -4,19 +4,19 @@ import { Plus } from "lucide-react";
 import { useParams, useRouter } from "next/navigation";
 
 import { Button } from "@/components/ui/button";
-// import { DataTable } from "@/components/ui/data-table";
+import { DataTable } from "@/components/ui/data-table";
 import { Heading } from "@/components/ui/heading";
 import { Separator } from "@/components/ui/separator";
-// import { ApiList } from "@/components/ui/api-list";
+import { ApiList } from "@/components/ui/api-list";
 
-// import { columns, BillboardColumn } from "./columns";
+import { columns, BillboardColumn } from "./columns";
 
 interface BillboardClientProps {
-    // data: BillboardColumn[];
+    data: BillboardColumn[];
 }
 
 export const BillboardClient: React.FC<BillboardClientProps> = ({
-    // data
+    data
 }) => {
     const params = useParams();
     const router = useRouter();
@@ -24,16 +24,16 @@ export const BillboardClient: React.FC<BillboardClientProps> = ({
     return (
         <>
             <div className="flex items-center justify-between">
-                {/* <Heading title={`Billboards (${data.length})`} description="Manage billboards for your store" /> */}
+                <Heading title={`Billboards (${data.length})`} description="Manage billboards for your store" />
                 <Button onClick={() => router.push(`/${params.storeId}/billboards/new`)}>
                     <Plus className="mr-2 h-4 w-4" /> Add New
                 </Button>
             </div>
             <Separator />
-            {/* <DataTable searchKey="label" columns={columns} data={data} /> */}
+            <DataTable searchKey="label" columns={columns} data={data} />
             <Heading title="API" description="API Calls for Billboards" />
             <Separator />
-            {/* <ApiList entityName="billboards" entityIdName="billboardId" /> */}
+            <ApiList entityName="billboards" entityIdName="billboardId" />
         </>
     );
 };
--- app/(dashboard)/[storeId]/(routes)/billboards/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/page.tsx
@@ -1,8 +1,8 @@
-// import { format } from "date-fns";
+import { format } from "date-fns";
 
 import prismadb from "@/lib/prismadb";
 
-// import { BillboardColumn } from "./components/columns"
+import { BillboardColumn } from "./components/columns"
 import { BillboardClient } from "./components/client";
 
 const BillboardsPage = async ({
@@ -19,16 +19,16 @@ const BillboardsPage = async ({
     }
   });
 
-//   const formattedBillboards: BillboardColumn[] = billboards.map((item) => ({
-//     id: item.id,
-//     label: item.label,
-//     createdAt: format(item.createdAt, 'MMMM do, yyyy'),
-//   }));
+  const formattedBillboards: BillboardColumn[] = billboards.map((item) => ({
+    id: item.id,
+    label: item.label,
+    createdAt: format(item.createdAt, 'MMMM do, yyyy'),
+  }));
 
   return (
     <div className="flex-col">
       <div className="flex-1 space-y-4 p-8 pt-6">
-        <BillboardClient  />
+        <BillboardClient data={formattedBillboards}  />
       </div>
     </div>
   );
